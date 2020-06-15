c839c9bb73c2f949e0e62aee34133d6fee71c793
Oleg Jakovlev <olegjakovlevlive@gmail.com>
2020-06-15 02:47:59 +0100

    Add project files.

---
+++ PHIT/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PHIT.Services;
+using PHIT.Views;
+
+namespace PHIT
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
